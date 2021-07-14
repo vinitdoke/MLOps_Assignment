4400c9c50f3d6824b548039902dec8457375834a
vinitdoke <54439587+vinitdoke@users.noreply.github.com>
2021-07-14 16:22:12 +0530

    Wrote Boiler Plate

---
+++ src/expt.py
@@ -0,0 +1,16 @@
+# import stuff
+
+# Read Data using DVC into Pandas
+
+
+# Perform EDA
+
+# Split Data and Store
+
+# Train Deciision Tree using Scikit
+
+# Save Trained Model
+
+# Evaluate Trained Model on test data
+
+# Save Accuracy and F1 Score into JSON in Metrics
